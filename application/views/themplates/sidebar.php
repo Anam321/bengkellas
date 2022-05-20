5cdadab2924b13fa47b30d4e851c3ce9e24d0106
anam321 <jackston9@gmail.com>
2022-05-21 01:02:58 +0700

    add template admin

---
+++ application/views/themplates/sidebar.php
@@ -0,0 +1,109 @@
+<div class="leftside-menu">
+
+    <!-- LOGO -->
+    <a href="index.html" class="logo text-center logo-light">
+        <span class="logo-lg">
+            <img src="<?= base_url() ?>assets/backend/images/logo.png" alt="" height="16">
+        </span>
+        <span class="logo-sm">
+            <img src="<?= base_url() ?>assets/backend/images/logo_sm.png" alt="" height="16">
+        </span>
+    </a>
+
+    <!-- LOGO -->
+    <a href="index.html" class="logo text-center logo-dark">
+        <span class="logo-lg">
+            <img src="<?= base_url() ?>assets/backend/images/logo-dark.png" alt="" height="16">
+        </span>
+        <span class="logo-sm">
+            <img src="<?= base_url() ?>assets/backend/images/logo_sm_dark.png" alt="" height="16">
+        </span>
+    </a>
+
+    <div class="h-100" id="leftside-menu-container" data-simplebar="">
+
+        <!--- Sidemenu -->
+        <ul class="side-nav">
+
+            <li class="side-nav-title side-nav-item">Navigation</li>
+
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="javascript:void(0)" aria-expanded="false"
+                    aria-controls="sidebarDashboards" class="side-nav-link">
+                    <i class="uil-home-alt"></i>
+
+                    <span> Dashboards </span>
+                </a>
+
+            </li>
+
+            <li class="side-nav-title side-nav-item">Apps</li>
+
+            <li class="side-nav-item">
+                <a href="<?= base_url('administrasi/profil') ?>" aria-expanded="false" aria-controls="sidebarEcommerce"
+                    class="side-nav-link">
+                    <i class="mdi mdi-account-lock "></i>
+                    <span> Profil </span>
+
+                </a>
+            </li>
+
+            <li class="side-nav-item">
+                <a href="<?= base_url('administrasi/katalog') ?>" aria-expanded="false" aria-controls="katalog"
+                    class="side-nav-link">
+                    <i class="mdi mdi-briefcase"></i>
+                    <span> Katalog </span>
+                  
+                </a>
+            </li>
+
+            <li class="side-nav-item">
+                <a href="<?= base_url('administrasi/gallery') ?>" aria-expanded="false" aria-controls="sastra"
+                    class="side-nav-link">
+                    <i class="mdi mdi-image-area"></i>
+                    <span> Gallery </span>
+                   
+                </a>
+            </li>
+            <li class="side-nav-item">
+                <a href="<?= base_url('administrasi/tender') ?>" aria-expanded="false" aria-controls="sastra"
+                    class="side-nav-link">
+                    <i class="mdi mdi-book-edit"></i>
+                    <span> Tender </span>
+                   
+                </a>
+            </li>
+
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarEcommerce" aria-expanded="false"
+                    aria-controls="sidebarEcommerce" class="side-nav-link">
+                    <i class="mdi mdi-monitor-cellphone"></i>
+                    <span> Web </span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sidebarEcommerce">
+                    <ul class="side-nav-second-level">
+                        <li>
+                            <a href="<?= base_url('identitasDesa') ?>">Artikel</a>
+                        </li>
+                        <li>
+                            <a href="<?= base_url('wilayahAdministrasi') ?>">Testimoni</a>
+                        </li>
+                        <li>
+                            <a href="apps-ecommerce-products-details.html">Contact</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+
+
+
+        </ul>
+
+        <div class="clearfix"></div>
+
+    </div>
+
+
+</div>
